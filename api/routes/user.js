f5e2745606af55b80078e862ef5a206af318c268
Bruno Guilherme da Silva <121551639+Bruno-Guilherme@users.noreply.github.com>
2025-06-11 13:49:38 -0300

    refact: userRoutes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,20 +1,39 @@
 import { Router } from "express";
 const router = Router();
 
+router.get('/email', async (req, res) => {
+    try {
+        const { email } = req.query;
+        if (!email) {
+            return res.status(400).json({ error: 'Email é obrigatório' });
+        }
+
+        const user = await req.context.models.User.findUserByEmail(email);
+
+        if (!user) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 router.get("/:id", async (req, res) => {
     const user = await req.context.models.User.findById(req.params.id);
     if (!user) return res.status(404).send({ message: "User not found." });
     return res.send(user);
 });
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
     const user = await req.context.models.User.findAllUsers();
     return res.send(user);
 });
 
 router.post("/", async (req, res) => {
     try {
-        const user = await req.context.models.User.createUser(req.body);
+        const user = await req.context.models.User.createUser(req.body, req.context.models);
         return res.status(201).send(user);
     } catch (error) {
         return res.status(400).send({ error: error.message });
